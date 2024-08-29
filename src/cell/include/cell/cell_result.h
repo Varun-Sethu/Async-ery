73ed1061bd43431ed5fb1df9df0c7d3a0219ec3a
Varun Sethu <varun.sethu@outlook.com>
2024-08-29 16:38:14 +1000

    type safety

--- src/cell/include/cell/cell_result.h
+++ src/cell/include/cell/cell_result.h
@@ -1,13 +1,14 @@
 #pragma once
 
 #include <variant>
+#include <concepts>
 
 namespace Cell {
     template <typename T, typename Err>
     using Result = std::variant<T, Err>;
 
-    template <typename T, typename Err, typename VisitT, typename VisitErr>
-    auto visit_result(Result<T, Err> result, VisitT visit_t, VisitErr visit_err) -> void {
+    template <typename T, typename Err, std::invocable<T> IfT, std::invocable<Err> IfErr>
+    auto visit_result(Result<T, Err> result, IfT visit_t, IfErr visit_err) -> void {
         if (std::holds_alternative<T>(result)) {
             visit_t(std::get<T>(result));
         } else {
@@ -15,8 +16,8 @@ namespace Cell {
         }
     }
 
-    template <typename T, typename Err, typename TFunc, typename ErrFunc>
-    auto map_result(Result<T, Err> result, TFunc t_func, ErrFunc err_func) -> decltype(err_func(std::get<Err>(result))) {
+    template <typename T, typename Err, std::invocable<T> IfT, std::invocable<Err> IfErr>
+    auto map_result(Result<T, Err> result, IfT t_func, IfErr err_func) -> decltype(auto) {
         if (std::holds_alternative<T>(result)) {
             return t_func(std::get<T>(result));
         }
